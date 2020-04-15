f339a7bb295f84abefeddc2380eeda0e5f5e6f28
AlexandreGuil <guillemotalexandre.eme@gmail.com>
2020-04-16 01:37:01 +0200

    @CrossOrigine Logger controler

--- src/main/java/com/inti/fromation/projetspring/ecommerce/controlers/LoggerControler.java
+++ src/main/java/com/inti/fromation/projetspring/ecommerce/controlers/LoggerControler.java
@@ -1,9 +1,11 @@
 package com.inti.fromation.projetspring.ecommerce.controlers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(value = "*")
 public class LoggerControler {
 
     @RequestMapping(value = "/login")
